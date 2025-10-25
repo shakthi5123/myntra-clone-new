891ce8447aee0be180204254b3718ff79ccf1bad
Shakthivel <shakthi@scripteco.com>
2025-10-25 17:06:34 +0530

    Fixed backend back to how it was

--- server/server.js
+++ server/server.js
@@ -26,9 +26,9 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-// const PORT = process.env.PORT || 3000
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`)
-// })
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
 
-export default app;
+// export default app;
